971f6c12cc8e943df471cbaaf123bbda36f5740f
Ria Hayati <147127122+pascaill@users.noreply.github.com>
2024-12-04 15:47:22 +0700

    Update inferenceService.js

--- services/inferenceService.js
+++ services/inferenceService.js
@@ -1,33 +1,34 @@
-const tf = require('@tensorflow/tfjs-node');
-const InputError = require('../exceptions/InputError');
- 
+const tf = require("@tensorflow/tfjs-node");
+const InputError = require("../exceptions/inputError.js");
+
 async function predictClassification(model, image) {
-    try {
-        const tensor = tf.node
-            .decodeJpeg(image)
-            .resizeNearestNeighbor([224, 224])
-            .expandDims()
-            .toFloat()
-  
-        const prediction = model.predict(tensor);
-        const score = await prediction.data();
-        const confidenceScore = Math.max(...score) * 100;
- 
-        const label = confidenceScore <= 50 ? 'Non-cancer' : 'Cancer'; 
-        let suggestion;
- 
-        if(label === 'Cancer') {
-            suggestion = "Segera periksa ke dokter!"
-        }
-        
-        if(label === 'Non-cancer') {
-            suggestion = "Anda sehat!"
-        }
- 
-        return { label, suggestion };
-    } catch (error) {
-        throw new InputError('Terjadi kesalahan dalam melakukan prediksi')
+  try {
+    const tensor = tf.node
+      .decodeJpeg(image)
+      .resizeNearestNeighbor([224, 224])
+      .expandDims()
+      .toFloat();
+
+    const prediction = model.predict(tensor);
+    const score = await prediction.data();
+    const confidenceScore = Math.max(...score) * 100;
+
+    if (confidenceScore > 50) {
+      result = "Cancer";
+      suggestion = "Segera periksa ke dokter!";
+    } 
+    
+    else {
+      result = 'Non-cancer';
+      suggestion = 'Penyakit kanker tidak terdeteksi.';
     }
+
+    return { result, suggestion };
+  } 
+  
+  catch (error) {
+    throw new InputError(`Terjadi kesalahan input: ${error.message}`);
+  }
 }
- 
+
 module.exports = predictClassification;
